764c64564ef23201fe0057179269167f92c5d693
Popalosh <popalosh@gmail.com>
2018-03-30 14:27:45 +0300

    Divided toWrite method to 2 methods

--- src/tar/Tar.java
+++ src/tar/Tar.java
@@ -36,7 +36,7 @@ public class Tar {
             }
 
 
-            toWrite(contents, new ArrayList<>(), "");
+            toWrite(contents);
 
         } else {
             if (args[0].equals("tar") && args[args.length - 2].equals("-out")) {
@@ -53,57 +53,55 @@ public class Tar {
 
                 toWrite(contents, sizes, args[args.length - 1]);
 
-            } else throw new IOException();
+            } else throw new Exception();
         }
     }
 
-    private static void toWrite(Map<String, ArrayList<String>> contents, List<Integer> sizes, String outputName) throws IOException {
+    private static void toWrite(Map<String, ArrayList<String>> contents) throws Exception {
 
-        if ((sizes.size() > 1)) {
+        for (String name : contents.keySet()) {
 
-            File file = new File("C:\\Users\\Popalosh\\IdeaProjects\\Java_polytech2\\output\\" + outputName);
+            File file = new File("C:\\Users\\Popalosh\\IdeaProjects\\Java_polytech2\\output\\" + name);
+
             BufferedWriter writer = Files.newBufferedWriter(Paths.get(file.toURI()));
 
-            writer.write(String.valueOf(sizes.size()));
-            writer.newLine();
-
-            int counter = 0;
-            int i = 0;
-
-            for (String name : contents.keySet()) {
-
-                writer.write(name + " " + sizes.get(i));
-                writer.newLine();
-                for (String line : contents.get(name)) {
-                    writer.write(line);
-                    if (contents.get(name).indexOf(line) != contents.get(name).size() - 1) {
-                        writer.newLine();
-                    }
-                }
-                if (counter < sizes.size()) {
+            for (String line : contents.get(name)) {
+                writer.write(line);
+                if (contents.get(name).indexOf(line) != contents.get(name).size() - 1) {
                     writer.newLine();
-                    counter++;
                 }
-                i++;
             }
             writer.close();
-
-        } else {
-
-            for (String name : contents.keySet()) {
-
-                File file = new File("C:\\Users\\Popalosh\\IdeaProjects\\Java_polytech2\\output\\" + name);
-
-                BufferedWriter writer = Files.newBufferedWriter(Paths.get(file.toURI()));
-
-                for (String line : contents.get(name)) {
-                    writer.write(line);
-                    if (contents.get(name).indexOf(line) != contents.get(name).size() - 1) {
-                        writer.newLine();
-                    }
-                }
-                writer.close();
-            }
         }
+    }
+
+    private static void toWrite(Map<String, ArrayList<String>> contents, List<Integer> sizes, String outputName) throws Exception {
+
+        File file = new File("C:\\Users\\Popalosh\\IdeaProjects\\Java_polytech2\\output\\" + outputName);
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get(file.toURI()));
+
+        writer.write(String.valueOf(sizes.size()));
+        writer.newLine();
+
+        int counter = 0;
+        int i = 0;
+
+        for (String name : contents.keySet()) {
+
+            writer.write(name + " " + sizes.get(i));
+            writer.newLine();
+            for (String line : contents.get(name)) {
+                writer.write(line);
+                if (contents.get(name).indexOf(line) != contents.get(name).size() - 1) {
+                    writer.newLine();
+                }
+            }
+            if (counter < sizes.size()) {
+                writer.newLine();
+                counter++;
+            }
+            i++;
+        }
+        writer.close();
     }
 }
